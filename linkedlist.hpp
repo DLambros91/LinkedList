cb152bfc891b552d02b700060f4b8c713fa5ee39
Demetrios <d.lambropoulos91@gmail.com>
2022-01-10 21:49:24 -0500

    Added empty check for linkedlist

--- linkedlist.hpp
+++ linkedlist.hpp
@@ -9,15 +9,24 @@ class node {
         node * next;
 };
 
+template <typename T>
+
 class linkedlist {
     private:
         int numNodes;
 
     public:
+        node<T> * head;
+        
+        // Returns number of data elements in the list
         int size() {
             return numNodes;
         }
-        
+
+        // Boolean returns true if empty
+        bool empty() {
+            return numNodes == 0;
+        }
 };
 
 #endif
